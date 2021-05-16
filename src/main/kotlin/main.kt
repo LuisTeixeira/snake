24006c2e0962fa141d8206c987e7d3d01d1f4a40
Luis Teixeira <teixeira13@gmail.com>
2021-05-16 22:03:10 +0200

    First commit - Kotlin js app working

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,43 @@
+import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.Window
+import org.w3c.dom.events.KeyboardEvent
+import kotlin.math.min
+import kotlin.random.Random
+
+fun main() {
+    val canvas = document.getElementById("snake-canvas") as HTMLCanvasElement
+    val game = Game(Random.Default)
+
+    window.onresize = {scaleCanvas(canvas, window); render(canvas, game)}
+
+    scaleCanvas(canvas, window)
+    render(canvas, game)
+
+    document.onkeydown = {onKeyDown(game, it)}
+    window.setInterval({game.step(); render(canvas, game)}, 100)
+
+}
+
+private fun scaleCanvas(canvas: HTMLCanvasElement, window: Window) {
+    val minDim = min(window.innerHeight, window.innerWidth - 1)
+    canvas.height = minDim
+    canvas.width = minDim
+
+    canvas.style.paddingTop =
+        ((window.innerHeight - minDim) / 2).toString() + "px"
+
+    canvas.style.paddingLeft =
+        ((window.innerWidth - minDim) / 2).toString() + "px"
+}
+
+private fun onKeyDown(game: Game, e: KeyboardEvent) {
+    when(e.key) {
+        "ArrowUp", "w" -> game.snake.turn(Direction.UP)
+        "ArrowDown", "s" -> game.snake.turn(Direction.DOWN)
+        "ArrowLeft", "a" -> game.snake.turn(Direction.LEFT)
+        "ArrowRight", "d" -> game.snake.turn(Direction.RIGHT)
+        " " -> if (!game.snake.alive) game.reset()
+    }
+}
